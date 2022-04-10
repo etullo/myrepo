47b50ef8807e9659a1c731e9d047d32cc17dff26
etullo <97974216+etullo@users.noreply.github.com>
2022-04-10 00:18:26 -0700

    midcourseassignment progress

---
+++ Midcourse_assignment.R
@@ -0,0 +1,14 @@
+---
+title: "Mid-course Assignment: Use all your skills"
+author: "Elizabeth Tullo"
+---
+
+## Mid-course Assignment: I <3 ggplot2
+
+Any text your write outside of code chunks
+```{r}
+```
+
+
+### Part 1: Loading in Data
+Here I am reading in the two data frames I will be working with for this assignment
